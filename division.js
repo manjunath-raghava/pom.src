1c156181070c7833c0864010488019aceaf91d11
manju <bobbychadwik@gmail.com>
2023-05-17 10:00:46 +0530

    divi opr

---
+++ division.js
@@ -0,0 +1,9 @@
+function divide()
+{
+var a = document.getElementById("first no").value
+var b = document.getElementById("second no").value
+var result
+result = a/b
+document.getElementById("result").value=result
+}
+
